8f44f8a3f6e107f9506ebde3dc49eb6def4f57c1
pauliuskalv <pauliuskalv@gmail.com>
2018-08-26 17:43:42 +0300

    Try to fix project library imports

---
+++ src/main/java/com/dnd_fan/dnd_program/model/Class.java
@@ -0,0 +1,8 @@
+package com.dnd_fan.dnd_program.model;
+
+public class Class {
+    private String mName;
+    private String mHitDie;
+    private Proficiency[] mProficiencies;
+    private String[] mSavingThrows;
+}
