427ddb40380ec4fbf30ed480847e31baf2f525a1
Phage <juokibildis@gmail.com>
2019-04-11 18:44:22 +0300

    Started implementing JSONFileInputStream.

--- src/main/java/lt/dndfan/dndhelper/persistence/json/impl/JSONFileInputStream.kt
+++ src/main/java/lt/dndfan/dndhelper/persistence/json/impl/JSONFileInputStream.kt
@@ -1,29 +1,59 @@
 package lt.dndfan.dndhelper.persistence.json.impl
 
 import lt.dndfan.dndhelper.persistence.json.IJSONInputStream
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.FileInputStream
+
 
 class JSONFileInputStream(
         override val filename: String
 ) : IJSONInputStream {
+
+    var fileInputStream = FileInputStream("")
+    var opened = false
+
     fun JSONFileInputStream(fileName : String) {
 
     }
 
     override fun open() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(!opened)
+            fileInputStream = FileInputStream(filename)
+            opened = true
+        // exception
     }
 
     override fun close() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(opened)
+            fileInputStream.close()
+            opened = false
+        // exception
     }
 
     override fun read(): List<Map<String, Any>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(opened) {
+            val mapList: MutableList<Map<String, Any>> = ArrayList()
+            val jsonArray = (fileInputStream.read() as JSONArray)
+
+            mapList.addAll(jsonArray as List<Map<String,Any>>)
+
+            return mapList
+        }
+        else
+        return emptyList()
     }
 
     override fun readAt(index: Int): Map<String, Any> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(opened) {
+            val map : MutableMap<String,Any>
+            val jsonArray = (fileInputStream.read() as JSONArray)
+            val jsonObject = jsonArray.get(index)
+
+            return jsonObject as Map<String,Any>
+        }
+        else
+            return emptyMap()
     }
 
     override fun getCount(): Int {
