25a4389da5510db53eeed28b42988241777c8ed7
Phage <juokibildis@gmail.com>
2019-04-23 23:12:09 +0300

    Tried to fix JSON parsing.

--- src/main/java/lt/dndfan/dndhelper/persistence/json/impl/JSONFileInputStream.kt
+++ src/main/java/lt/dndfan/dndhelper/persistence/json/impl/JSONFileInputStream.kt
@@ -1,5 +1,7 @@
 package lt.dndfan.dndhelper.persistence.json.impl
 
+import jdk.nashorn.internal.parser.JSONParser
+import jdk.nashorn.internal.runtime.Context
 import lt.dndfan.dndhelper.persistence.json.IJSONInputStream
 import org.json.JSONArray
 import org.json.JSONObject
@@ -10,8 +12,8 @@ class JSONFileInputStream(
         override val filename: String
 ) : IJSONInputStream {
 
-    var fileInputStream = FileInputStream("")
     var opened = false
+    val jsonParser = JSONParser(filename, Context.getGlobal(), false)
 
     fun JSONFileInputStream(fileName : String) {
 
@@ -19,26 +21,27 @@ class JSONFileInputStream(
 
     override fun open() {
         if(!opened)
-            fileInputStream = FileInputStream(filename)
-            opened = true
-        // exception
+            // should probably change name from open to available
+            // since json parser opens the file in constructor
+            if(FileInputStream(filename).available() > 0)
+                opened = true
+        // exception?
     }
 
     override fun close() {
         if(opened)
-            fileInputStream.close()
             opened = false
-        // exception
     }
 
     override fun read(): List<Map<String, Any>> {
         if(opened) {
-            val mapList: MutableList<Map<String, Any>> = ArrayList()
-            val jsonArray = (fileInputStream.read() as JSONArray)
+            val jsonArray : JSONArray
+            // unsure if cast is going to work
+            jsonArray = jsonParser.parse() as JSONArray
 
-            mapList.addAll(jsonArray as List<Map<String,Any>>)
-
-            return mapList
+            // very unsure if this cast is going to work
+            // consider separating JSONArray into JSONObjects and then casting them
+            return jsonArray as List<Map<String,Any>>
         }
         else
         return emptyList()
@@ -47,7 +50,9 @@ class JSONFileInputStream(
     override fun readAt(index: Int): Map<String, Any> {
         if(opened) {
             val map : MutableMap<String,Any>
-            val jsonArray = (fileInputStream.read() as JSONArray)
+            val jsonArray : JSONArray
+            jsonArray = jsonParser.parse() as JSONArray
+
             val jsonObject = jsonArray.get(index)
 
             return jsonObject as Map<String,Any>
@@ -57,7 +62,11 @@ class JSONFileInputStream(
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // return the remaining bytes in the stream
+        // there should be a better way to implement this
+        if(opened)
+            return FileInputStream(filename).available()
+        return 0
     }
 
     override fun getIterator(): Iterator<Map<String, Any>> {
