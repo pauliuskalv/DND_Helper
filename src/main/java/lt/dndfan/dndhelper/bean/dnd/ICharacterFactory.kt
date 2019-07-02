dd5a1f36b30dbd9397cdacdcbabc1e5d014bf598
Phage <juokibildis@gmail.com>
2019-07-02 19:06:35 +0300

    Implemented CharacterDomain and CharacterFactory.

---
+++ src/main/java/lt/dndfan/dndhelper/bean/dnd/ICharacterFactory.kt
@@ -0,0 +1,102 @@
+package lt.dndfan.dndhelper.bean.dnd
+
+import lt.dndfan.dndhelper.bean.dnd.constant.EAlignment
+import lt.dndfan.dndhelper.bean.dnd.constant.ESpellSlot
+import lt.dndfan.dndhelper.bean.dnd.feature.ITrait
+import lt.dndfan.dndhelper.bean.dnd.inventory.ICharacterInventory
+import lt.dndfan.dndhelper.bean.dnd.spell.IAbility
+import lt.dndfan.dndhelper.bean.dnd.spell.ISpellPool
+import lt.dndfan.dndhelper.bean.dnd.stats.impl.Stat
+import lt.dndfan.dndhelper.util.collection.IPair
+
+interface ICharacterFactory {
+    fun createCharacter( allStats: List<Stat>,
+                        level : Int,
+                        armorClass: Int,
+                        initiative: Int,
+                        speed: Int,
+                        swimmingSpeed: Int,
+                        flyingSpeed: Int,
+                        alignment: EAlignment,
+                        hitDiceCount: Int,
+                        currentHitDice: Int,
+                        name: String,
+                        description: String,
+                        traits: List<ITrait>,
+                        temporaryHitPoints: Int,
+                        maxHitPoints: Int,
+                        currentHitPoints: Int,
+                        inspiration: Boolean,
+                        inspirationDie: Int) : ICharacter
+
+    fun createPlayableCharacter(allStats: List<Stat>,
+                                level : Int,
+                                armorClass: Int,
+                                initiative: Int,
+                                speed: Int,
+                                swimmingSpeed: Int,
+                                flyingSpeed: Int,
+                                alignment: EAlignment,
+                                hitDiceCount: Int,
+                                currentHitDice: Int,
+                                name: String,
+                                description: String,
+                                traits: List<ITrait>,
+                                abilities: List<IAbility>,
+                                spellModifier: Stat,
+
+                                temporaryHitPoints: Int,
+                                maxHitPoints: Int,
+                                currentHitPoints: Int,
+
+                                characterClass: String,
+                                characterSubclass: String,
+
+                                race: String,
+                                subrace: String,
+
+                                characterInventory : ICharacterInventory,
+
+                                inspiration: Boolean,
+                                inspirationDie: Int) : IPlayableCharacter
+
+    fun createSpellcaster(allStats: List<Stat>,
+                          level : Int,
+                          armorClass: Int,
+                          initiative: Int,
+                          speed: Int,
+                          swimmingSpeed: Int,
+                          flyingSpeed: Int,
+                          alignment: EAlignment,
+                          hitDiceCount: Int,
+                          currentHitDice: Int,
+                          name: String,
+                          description: String,
+                          traits: List<ITrait>,
+                          abilities: List<IAbility>,
+                          spellModifier: Stat,
+
+                          temporaryHitPoints: Int,
+                          maxHitPoints: Int,
+                          currentHitPoints: Int,
+
+                          characterClass: String,
+                          characterSubclass: String,
+
+                          race: String,
+                          subrace: String,
+
+                          personalityTraits: List<String>,
+                          ideals: List<String>,
+                          bonds: List<String>,
+                          flaws: List<String>,
+
+                          characterInventory : ICharacterInventory,
+
+                          spellsKnown: Int,
+                          cantripsKnown: Int,
+                          spellSlots: List<IPair<ESpellSlot, Int>>,
+                          spellPool: ISpellPool,
+                          inspiration: Boolean,
+                          inspirationDie: Int) : ISpellcaster
+}
---
+++ src/main/java/lt/dndfan/dndhelper/domain/dnd/character/impl/CharacterDomain.kt
@@ -0,0 +1,124 @@
+package lt.dndfan.dndhelper.domain.dnd.character.impl
+
+import lt.dndfan.dndhelper.bean.dnd.ICharacter
+import lt.dndfan.dndhelper.bean.dnd.IPlayableCharacter
+import lt.dndfan.dndhelper.bean.dnd.ISpellcaster
+import lt.dndfan.dndhelper.bean.dnd.constant.EAlignment
+import lt.dndfan.dndhelper.bean.dnd.constant.ESpellSlot
+import lt.dndfan.dndhelper.bean.dnd.feature.ITrait
+import lt.dndfan.dndhelper.bean.dnd.impl.CharacterFactory
+import lt.dndfan.dndhelper.bean.dnd.inventory.ICharacterInventory
+import lt.dndfan.dndhelper.bean.dnd.spell.IAbility
+import lt.dndfan.dndhelper.bean.dnd.spell.ISpellPool
+import lt.dndfan.dndhelper.bean.dnd.stats.impl.Stat
+import lt.dndfan.dndhelper.domain.dnd.character.ICharacterDomain
+import lt.dndfan.dndhelper.util.collection.IPair
+
+class CharacterDomain : ICharacterDomain {
+    override fun createCharacter(args : Map<String, Any>): ICharacter {
+        val characterFactory = CharacterFactory()
+        return characterFactory.createCharacter(
+                args["AllStats"] as List<Stat>,
+                args["level "] as Int,
+                args["armorClass"] as Int,
+                args["initiative"] as Int,
+                args["speed"] as Int,
+                args["swimmingSpeed"] as Int,
+                args["flyingSpeed"] as Int,
+                args["alignment"] as EAlignment,
+                args["hitDiceCount"] as Int,
+                args["currentHitDice"] as Int,
+                args["name"] as String,
+                args["description"] as String,
+                args["traits"] as List<ITrait>,
+                args["temporaryHitPoints"] as Int,
+                args["maxHitPoints"] as Int,
+                args["currentHitPoints"] as Int,
+                args["inspiration"] as Boolean,
+                args["inspirationDie"] as Int)
+    }
+
+    override fun createPlayableCharacter(args : Map<String, Any>): IPlayableCharacter {
+        val characterFactory = CharacterFactory()
+        return characterFactory.createPlayableCharacter(args["allStats"] as List<Stat>,
+                args["level"] as Int,
+                args["armorClass"] as Int,
+                args["initiative"] as Int,
+                args["speed"] as Int,
+                args["swimmingSpeed"] as Int,
+                args["flyingSpeed"] as Int,
+                args["alignment"] as EAlignment,
+                args["hitDiceCount"] as Int,
+                args["currentHitDice"] as Int,
+                args["name"] as String,
+                args["description"] as String,
+                args["traits"] as List<ITrait>,
+                args["abilities"] as List<IAbility>,
+                args["spellModifier"] as Stat,
+                args["temporaryHitPoints"] as Int,
+                args["maxHitPoints"] as Int,
+                args["currentHitPoints"] as Int,
+                args["characterClass"] as String,
+                args["characterSubclass"] as String,
+                args["race"] as String,
+                args["subrace"] as String,
+                args["characterInventory "] as ICharacterInventory,
+                args["inspiration"] as Boolean,
+                args["inspirationDie"] as Int)
+    }
+
+    override fun createSpellCaster(args:  Map<String, Any>): ISpellcaster {
+        val characterFactory = CharacterFactory()
+        return characterFactory.createSpellcaster(
+                args["allStats"] as List<Stat>,
+                args["level "] as Int,
+                args["armorClass"] as Int,
+                args["initiative"] as Int,
+                args["speed"] as Int,
+                args["swimmingSpeed"] as Int,
+                args["flyingSpeed"] as Int,
+                args["alignment"] as EAlignment,
+                args["hitDiceCount"] as Int,
+                args["currentHitDice"] as Int,
+                args["name"] as String,
+                args["description"] as String,
+                args["traits"] as List<ITrait>,
+                args["abilities"] as List<IAbility>,
+                args["spellModifier"] as Stat,
+
+                args["temporaryHitPoints"] as Int,
+                args["maxHitPoints"] as Int,
+                args["currentHitPoints"] as Int,
+
+                args["characterClass"] as String,
+                args["characterSubclass"] as String,
+
+                args["race"] as String,
+                args["subrace"] as String,
+
+                args["personalityTraits"] as List<String>,
+                args["ideals"] as List<String>,
+                args["bonds"] as List<String>,
+                args["flaws"] as List<String>,
+
+                args["characterInventory "] as ICharacterInventory,
+
+                args["spellsKnown"] as Int,
+                args["cantripsKnown"] as Int,
+                args["spellSlots"] as List<IPair<ESpellSlot, Int>>,
+                args["spellPool"] as ISpellPool,
+                args["inspiration"] as Boolean,
+                args["inspirationDie"] as Int
+        )
+    }
+
+    override fun getCharacterByName(characterList: List<ICharacter>, name: String): ICharacter {
+        for (character in characterList) {
+            if(character.name == name) {
+                return character
+            }
+        }
+        /** TODO: Exception */
+        return characterList[0]
+    }
+}
