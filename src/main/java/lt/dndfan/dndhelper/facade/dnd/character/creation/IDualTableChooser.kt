bb008cce7f23a77cad2ea357ec66a2b759a935cf
Phage <juokibildis@gmail.com>
2019-07-03 09:05:20 +0300

    Updated IDualTableChooser and implemented SpellChooser.

--- src/main/java/lt/dndfan/dndhelper/facade/dnd/character/creation/IDualTableChooser.kt
+++ src/main/java/lt/dndfan/dndhelper/facade/dnd/character/creation/IDualTableChooser.kt
@@ -2,36 +2,83 @@ package lt.dndfan.dndhelper.facade.dnd.character.creation
 
 /**
  * Used for spell or, maybe, starting items choosers.
+ * Not sure if implementing the transfer functions here is a good idea.
  */
 
 interface IDualTableChooser {
 
     /** Table of possible object choices */
-    val choiceTable : List<Any>
+    val choiceTable : ArrayList<Any>
     /** Table of chosen objects */
-    val chosenTable : List<Any>
+    val chosenTable : ArrayList<Any>
     /** Number of items that can be added to the chosenTable */
     val selectableObjects : Int
 
     /**
-     * Transfer the selected object to the other table.
+     * Transfer the selected object from choices to the desired choices table.
      * That means remove from one table and add it to the other one.
      */
-    fun transfer(transferee : Any)
+    private fun transferToChosen(transferee : Any) {
+        if(canTransferToChosen(transferee)) {
+            chosenTable.add(transferee)
+            choiceTable.remove(transferee)
+        }
+    }
+
+    private fun transferToChoices(transferee : Any)  {
+        if(canTransferFromChoices(transferee)) {
+            choiceTable.add(transferee)
+            chosenTable.remove(transferee)
+        }
+    }
+
     /**
-     * Check if the transfer is allowed .
-     * Can not transfer when selectable object limit is reached
+     * Check if the transfer is allowed.
+     * Can not transfer when selectable object limit is reached.
      */
-    fun canTransfer(transferee : Any) : Boolean
+    private fun canTransferToChosen(transferee : Any) : Boolean {
+        if(choiceTable.contains(transferee) &&
+                !chosenTable.contains(transferee) &&
+                 chosenTable.size < selectableObjects) {
+                return true
+        }
+        return false
+    }
+
+    /** There is no limit of objects in the choice list */
+    private fun canTransferFromChoices(transferee: Any) : Boolean {
+        if(chosenTable.contains(transferee) &&
+                !choiceTable.contains(transferee)) {
+                return true
+        }
+        return false
+    }
+
+    /**
+     * Is the correct number of objects selected?
+     * It is needed in cases where to few objects are selected
+     */
+    fun canApply() : Boolean {
+        if(chosenTable.size == selectableObjects)
+            return true
+        return false
+    }
+
+    fun transfer(transferee : Any) {
+        /** The object is in choices */
+        if(canTransferToChosen(transferee)) {
+            transferToChosen(transferee)
+        }
+        /** Technically this check should not be needed */
+        if(canTransferFromChoices(transferee)) {
+            transferToChosen(transferee)
+        }
+    }
 
     /**
      * Function that is called to apply the objects from the chosenTable to the playableCharacter.
+     * Should call canApply().
      * Should call addSpell from domain package for example.
      */
     fun applyChanges()
-
-    /**
-     * Is it possible to continue?
-     */
-    fun canApply() : Boolean
 }
